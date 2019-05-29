ff5a6271bc6f57f42258d3b9344f6a83761eabc6
SudarshanKumar143341 <48338961+SudarshanKumar143341@users.noreply.github.com>
2019-05-29 20:09:13 +0930

    Signed-off-by: SudarshanKumar143341 <48338961+SudarshanKumar143341@users.noreply.github.com> js for toggle navigation and form

---
+++ js/myscript.js
@@ -0,0 +1,18 @@
+
+function toggleSidebar(){
+  document.getElementById("sidebar").classList.toggle('active');
+}
+function on() {
+  document.getElementById("navigation").style.display = "block";
+}
+
+function off() {
+  document.getElementById("navigation").style.display = "none";
+}
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
